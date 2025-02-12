99684cc2774eee658dfb0f6100c7d94f72182953
charroux <benoit.charroux@gmail.com>
2025-02-12 15:08:36 +0100

    put

--- rent/src/main/java/com/example/rent/web/RentalService.java
+++ rent/src/main/java/com/example/rent/web/RentalService.java
@@ -1,17 +1,32 @@
 package com.example.rent.web;
 
+import com.example.rent.data.Car;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class RentalService {
 
+    List<Car> cars = new ArrayList<Car>();
     Logger logger = LoggerFactory.getLogger(RentalService.class);
+
+    public RentalService() {
+        Car car = new Car("Ferrari", 1000);
+        cars.add(car);
+    }
 
     @GetMapping("/bonjour")
     public String disBonjour(){
         return "Bonjour";
+    }
+
+    @GetMapping("/cars")
+    public List<Car> getCars(){
+        return cars;
     }
 
     @PutMapping(value = "/cars/{plateNumber}")
